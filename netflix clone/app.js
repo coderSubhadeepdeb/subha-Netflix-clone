c74ff6bb404e2ecea3f0689441dd87e6ab7af02f
coderSubhadeepdeb <sbhdpdeb@gmail.com>
2024-06-20 03:43:29 +0530

    Add files via upload

---
+++ netflix clone/app.js
@@ -0,0 +1,35 @@
+ let port= document.body.getElementsByClassName('options')
+ let porttwo= document.body.getElementsByClassName('optionhidden')
+ let portthree= document.body.getElementsByClassName('plusplus')
+
+ port[0].addEventListener('click', ()=>{
+      porttwo[0].style.display = porttwo[0].style.display=='flex'?'none':'flex';
+      portthree[0].style.transform = portthree[0].style.transform == 'rotateZ(45deg)'? 'rotateZ(0)':'rotateZ(45deg)';
+ }
+ )
+ port[1].addEventListener('click', ()=>{
+      porttwo[1].style.display = porttwo[1].style.display=='flex'?'none':'flex';
+      portthree[1].style.transform = portthree[1].style.transform == 'rotateZ(45deg)'? 'rotateZ(0)':'rotateZ(45deg)';
+ })
+ 2
+ port[2].addEventListener('click', ()=>{
+      porttwo[2].style.display = porttwo[2].style.display=='flex'?'none':'flex';
+      portthree[2].style.transform = portthree[2].style.transform == 'rotateZ(45deg)'? 'rotateZ(0)':'rotateZ(45deg)';
+ })
+ 3
+ port[3].addEventListener('click', ()=>{
+      porttwo[3].style.display = porttwo[3].style.display=='flex'?'none':'flex';
+      portthree[3].style.transform = portthree[3].style.transform == 'rotateZ(45deg)'? 'rotateZ(0)':'rotateZ(45deg)';
+ })
+ 4
+ port[4].addEventListener('click', ()=>{
+      porttwo[4].style.display = porttwo[4].style.display=='flex'?'none':'flex';
+      portthree[4].style.transform = portthree[4].style.transform == 'rotateZ(45deg)'? 'rotateZ(0)':'rotateZ(45deg)';
+ }
+ )
+ port[5].addEventListener('click', ()=>{
+      porttwo[5].style.display = porttwo[5].style.display=='flex'?'none':'flex';
+      portthree[5].style.transform = portthree[5].style.transform == 'rotateZ(45deg)'? 'rotateZ(0)':'rotateZ(45deg)';
+ }
+ )
+ 
